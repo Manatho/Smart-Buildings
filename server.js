048e327d11d1657bf1b8ef28fafae2f0541d2393
Unknown <mathies-h@hotmail.com>
2019-05-16 11:40:12 +0200

    Changed format of get data and added in people schedule

--- server.js
+++ server.js
@@ -7,38 +7,69 @@ const data = require("./public/weatherdata.json");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-// /data?day=1557846833
+// /data?day=yyyy/mm/dd&profile=[profile]
 app.get("/data", (req, res) => {
-	let day = new Date(req.query.day*1000);
-	day.setHours(2);
-	day.setMinutes(0);
-	day.setSeconds(0);
-	day.setMilliseconds(0);
-	day = day.getTime() / 1000;
-	
-	let profile = req.query.profile;
-	let people = 0;
-	if(profile == "office"){
-		people = 5;
-	} else {
-		people = 1;
-	}
+	let day = new Date(req.query.day);
+	day.setHours(1); //Convert to UTC
 
+	let profile = req.query.profile;
+	let people = getPeopleOfTheDay(profile, day);
+	
+	day = day.getTime() / 1000; //Convert to epoch in seconds
+	
+	//Get the weather data for the day
 	let dataSlice;
 	for (let i = 0; i < data.length; i++) {
 		const dp = data[i];
 		if (dp.time < day && dp.time > day - 600) {
-			dataSlice = data.slice(i, i+145);
+			dataSlice = data.slice(i, i+144);
 			break;
 		}
 	}
+
+	//dataslice was never set
+	if(dataSlice == null){
+		res.send("Error, date out of bound");
+		return;
+	}
+
+	//Create dataformat for BMS: [time](temperature){people}
 	let response = "";
-	dataSlice.forEach(element => {
-		response += `[${element.time}]{${element.temperature.toFixed(2)}}(${people})\n`
+	dataSlice.forEach((element, i) => {
+		let time = new Date(element.time*1000);
+		time.setHours(time.getHours()-1); // Convert to UTC
+		time.setMinutes(time.getMinutes()+9); // Minute Offset to align with 00:00
+		let timeString = time.getHours() + ":" + time.getMinutes();
+
+		response += `[${timeString}](${element.temperature.toFixed(2)}){${people[i]}}\n`
 	});
 	res.send(response);
-    console.log("---------------");
 });
+
+const samplePrHour = 6;
+const weekDay = {"0": "Sunday", "1":"Monday", "2":"Tuesday", "3":"Wednesday", "4":"Thursday", "5":"Friday", "6":"Saturday"}
+function getPeopleOfTheDay(profile, day){
+	let people = new Array(144) // update every 10 minutes
+	
+	for (let i = 0; i < people.length; i++) {
+		if(profile == "office"){
+			if(i < 8*samplePrHour || i > 16*samplePrHour || weekDay[day.getDay()] == "Saturday" || weekDay[day.getDay()] == "Sunday"){ 
+				people[i] = 0;
+			} else {
+				people[i] = 1;
+			}
+		} else {
+			if(i < 8*samplePrHour || i > 16*samplePrHour || weekDay[day.getDay()] == "Saturday" || weekDay[day.getDay()] == "Sunday"){ 
+				people[i] = 0;
+			} else {
+				people[i] = 10;
+			}
+		}
+	}
+	return people;
+}
+
+
 
 app.post("/predict", (req, res) => {
 	// BMS: the data of the day + timestep
